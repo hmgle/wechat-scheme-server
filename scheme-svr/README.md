66f955bddc198bb8456c83030ac24a7bfa363d0f
mingang.he <dustgle@gmail.com>
2015-09-14 20:59:30 +0800

    update

--- scheme-svr/README.md
+++ scheme-svr/README.md
@@ -1,5 +1,6 @@
 # How to run?
 
 ```console
+$ mkfifo /tmp/scmfifo
 $ nohup ./bootstrap.sh > /dev/null 2>&1 &
 ```
--- scheme-svr/bootstrap.sh
+++ scheme-svr/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkfifo /tmp/scmfifo
+[[ -p /tmp/scmfifo ]] || mkfifo /tmp/scmfifo
 
 while true; do
   cat /tmp/scmfifo | yascm/yascm 2>/dev/null | nc -l -p 2345 > /tmp/scmfifo
