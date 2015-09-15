928debdc49f0015035922809dc1e36840102f4d0
mingang.he <dustgle@gmail.com>
2015-09-15 12:20:32 +0800

    fix

--- scheme-svr/bootstrap.sh
+++ scheme-svr/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 [[ -p /tmp/scmfifo ]] || mkfifo /tmp/scmfifo
+cd yascm
 
 while true; do
-  cat /tmp/scmfifo | yascm/yascm 2>/dev/null | nc -l -p 2345 > /tmp/scmfifo
+  cat /tmp/scmfifo | ./yascm 2>/dev/null | nc -l -p 2345 > /tmp/scmfifo
 done
